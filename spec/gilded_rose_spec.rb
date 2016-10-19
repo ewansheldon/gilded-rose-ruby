51b8f8305b9b0f7281a604fc405a335526bc3b40
Ewan Sheldon <ewansheldon@gmail.com>
2016-10-19 14:18:40 +0100

    quality double rate after sell by test

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -5,13 +5,20 @@ require File.join(File.dirname(__FILE__), '../item')
 describe GildedRose do
   # let(:item) {double :item, name: 'apple', sell_in: 5, quality: 15}
 
-  items = [Item.new('apple', 5, 20)]
-  subject(:gilded_rose) {described_class.new(items)}
+  let(:item) {Item.new('apple', 5, 20)}
+  subject(:gilded_rose) {described_class.new([item])}
 
   describe "#update_quality" do
     it "does not change the name" do
-      gilded_rose.update_quality()
-      expect(items[0].name).to eq "apple"
+      gilded_rose.update_quality
+      expect(item.name).to eq "apple"
+    end
+
+    it 'degrades quality twice as quickly after sell by date has passed' do
+      5.times {gilded_rose.update_quality}
+      expect(item.quality).to eq 15
+      gilded_rose.update_quality
+      expect(item.quality).to eq 13
     end
   end
 end
